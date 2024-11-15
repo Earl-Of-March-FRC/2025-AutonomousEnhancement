dc23a1ae5eb43b4147b54825dbbd4e4682fd9635
Wilson <wilsonyu657@gmail.com>
2024-11-14 19:14:55 -0500

    Kelvin

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -2,7 +2,7 @@ package frc.robot;
 
 public class Constants {
   public static class VisionConstants {
-    public static final String CAMERA_NAME = "Camera_Module_v1i";
+    public static final String CAMERA_NAME = "Hello";
 
     public static final double CAMERA_TO_ROBOT_X = 0;
     public static final double CAMERA_TO_ROBOT_Y = 0;
