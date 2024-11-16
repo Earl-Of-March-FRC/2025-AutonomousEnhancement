e13dbd1ca7c3027cd6a0bc227e18cc4cefb7634f
elliottstarosta <starlliott@gmail.com>
2024-11-16 16:26:02 -0500

    added hashmap import

--- src/main/java/frc/robot/Subsystems/VisionSubsystem.java
+++ src/main/java/frc/robot/Subsystems/VisionSubsystem.java
@@ -19,6 +19,7 @@ import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.VisionConstants;
+import java.util.HashMap;
 
 public class VisionSubsystem extends SubsystemBase {
   private final PhotonCamera camera;
