0b09fcc3f51ea81c7ed95d6f37d838dd2b968035
RP-12000 <kelvin.hby@outlook.com>
2024-11-26 19:58:08 -0500

    Initial Commit

--- src/main/java/frc/robot/Subsystems/DrivetrainSubsystem.java
+++ src/main/java/frc/robot/Subsystems/DrivetrainSubsystem.java
@@ -11,7 +11,6 @@ import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class DrivetrainSubsystem extends SubsystemBase {
