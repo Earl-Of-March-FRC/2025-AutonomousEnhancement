3403b4fbf85ee68e1aeedc0872583c12958d5705
Aviweiam <nika.pomazova@gmail.com>
2024-11-28 20:15:18 -0500

    added encoder and tank drive commands

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,20 +5,34 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.Commands.ApriltagAlign;
 import frc.robot.Commands.TargetAlign;
+import frc.robot.Constants.DriverConstants;
+import frc.robot.Subsystems.ArmSubsystem;
 import frc.robot.Subsystems.DrivetrainSubsystem;
 import frc.robot.Subsystems.VisionSubsystem;
 
 public class RobotContainer { 
+
+  private final CommandXboxController driver = new CommandXboxController(DriverConstants.DRIVER_PORT);
+
   private final VisionSubsystem visionSub = new VisionSubsystem();
   private final DrivetrainSubsystem driveSub = new DrivetrainSubsystem();
+  private final ArmSubsystem armSub = new ArmSubsystem();
 
   public RobotContainer() {
-    
+    driveSub.setDefaultCommand(
+      new TankDriveCmd(driveSub, () -> driveSub.radicalSpeed(driver.getRawAxis(DriverConstants.LEFT_AXIS)), () -> driveSub.radicalSpeed(driver.getRawAxis(DriverConstants.RIGHT_AXIS)))
+    );
   }
 
 
   public Command getAutonomousCommand() {
     return new TargetAlign(driveSub, visionSub);
+  }
+
+  public void configureBindings() {
+    driver.x().whileTrue(new ApriltagAlign(armSub, visionSub));
   }
 }
--- src/main/java/frc/robot/Subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/Subsystems/ArmSubsystem.java
@@ -4,14 +4,57 @@
 
 package frc.robot.Subsystems;
 
+import edu.wpi.first.wpilibj.Encoder;
+
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.ArmConstants;
 
 public class ArmSubsystem extends SubsystemBase {
+  
+  private final WPI_TalonSRX rightLift = new WPI_TalonSRX(ArmConstants.LEFT_MOTOR_ID);
+  private final WPI_TalonSRX leftLift = new WPI_TalonSRX(ArmConstants.RIGHT_MOTOR_ID);
+
+  private final Encoder encoder = new Encoder(0, 2);
+
   /** Creates a new ArmSubsystem. */
-  public ArmSubsystem() {}
+  public ArmSubsystem() {
+    leftLift.follow(rightLift);
+
+    leftLift.setInverted(true);
+    rightLift.setInverted(false);
+
+    leftLift.setNeutralMode(NeutralMode.Brake);
+    rightLift.setNeutralMode(NeutralMode.Brake);
+
+    encoder.setDistancePerPulse(1.00/16.00);
+  }
 
   @Override
   public void periodic() {
+    SmartDashboard.putNumber("Motor speed", rightLift.get());
     // This method will be called once per scheduler run
+  }
+
+  public void setSpeed(double speed) {
+    rightLift.set(MathUtil.clamp(speed, -ArmConstants.MAX_SPEED, ArmConstants.MAX_SPEED));
+  }
+
+  public double getArmPosition() {
+    return encoder.getDistance();
+  }
+
+  public double getEncoderRate() {
+    return encoder.getRate();
+  }
+
+  public void stopMotors(){
+    leftLift.stopMotor();
+    rightLift.stopMotor();
   }
 }
